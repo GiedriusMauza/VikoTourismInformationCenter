113c3b80a7781150147b193bff245956489bcbe1
GiedriusMauza <giedrius.mauza@gmail.com>
2023-04-13 19:39:10 +0300

    Razor scaffolding added.

--- VikoTourismInformationCenter/Program.cs
+++ VikoTourismInformationCenter/Program.cs
@@ -14,7 +14,9 @@ namespace VikoTourismInformationCenter
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+/*            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();*/
 
 /*            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();*/
@@ -28,6 +30,7 @@ namespace VikoTourismInformationCenter
                 options.Lockout.MaxFailedAccessAttempts = 5;
             });
             builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
@@ -48,12 +51,16 @@ namespace VikoTourismInformationCenter
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
+
+
 
             app.Run();
         }
